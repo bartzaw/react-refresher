ec658998e5bb1afc36cda805e3c1434dbc522c17
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-12-15 17:29:37 +0100

    version with constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    )
-    return <div>Hi there!</div>
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        )
+
+        return <div>Latitude: </div>
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
