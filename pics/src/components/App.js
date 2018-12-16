9002124acf38e11aaa7d5fab2722aadd0f280708
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-12-16 21:35:58 +0100

    allow onsearchsubmit to set new state and show number of pics from server

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,20 +4,23 @@ import SearchBar from './SearchBar'
 
 
 class App extends React.Component {
-    async onSearchSubmit(term) {
+    state = { images: [] }
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 58fe113ec5ed697ddb8a376a3bbf634dab6613cb35467de6a54a904279408976',
             }
         })
-        console.log(response.data.results)
+        this.setState({ images: response.data.results })
     }
-
+ 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         )
     }
