0d0a46cfa2139604f8a22cc8647d3dc750154780
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-12-16 20:52:55 +0100

    add axios to fetch data from unsplash to onsearchsubmit function

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './SearchBar'
+
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term)
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 58fe113ec5ed697ddb8a376a3bbf634dab6613cb35467de6a54a904279408976',
+            },
+        })
     }
 
     render() {
