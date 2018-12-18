1d2028544f9646480509b7b687be9f9967bd7d09
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-12-18 22:43:25 +0100

    add SearchBar component with boiler plate inside

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import SearchBar from './SearchBar'
 
 class App extends Component {
     render() {
         return (
-            <div>App</div>
+            <SearchBar />
         )
     }
 }
