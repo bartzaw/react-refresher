0bef886e1943a80de617f068c30c3d66ccebd437
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-12-18 22:47:06 +0100

    add css and jsx input to searchbar component

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class SearchBar extends Component {
     render() {
         return (
-            <div>Search Bar</div>
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text"/>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
