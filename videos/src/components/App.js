a42c13b3a9d5bd6623ee6e26f980e11bf8028862
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-12-19 23:04:04 +0100

    add async response from yt and add them into state

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar'
+import youtube from '../apis/youtube'
 
 class App extends Component {
-    onTermSubmit = (term) => {
-        console.log(term)
+    state = { videos: [] }
+
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        })
+        
+        this.setState({ videos: response.data.items })
     }
 
     render() {
