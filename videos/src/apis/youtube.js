606c1cdb699e4664a81c7e578b8acffe8a03b072
Bartosz Zawada <bartosh.zavada@gmail.com>
2018-12-19 22:20:20 +0100

    add axios and its config to youtube.js file

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios'
+
 const KEY = 'AIzaSyBZ7H2RLDXcQNBEwCwwt59tHFQCThHYe_Y'
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: { 
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY,
+     }
+
+})
